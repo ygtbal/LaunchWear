120ecef22b72cd6395bb75a491d51a4c8924132b
ygtbal <yigitbalalan@gmail.com>
2025-03-03 23:32:31 +0300

    LauchWear first commit

---
+++ lib/constants/index.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Launchwear";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A clothing store";
+export const APP_URL =
+  process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
