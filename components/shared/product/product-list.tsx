c66568a2771c94ac7ec26ef670d1a76b485ee6a3
ygtbal <yigitbalalan@gmail.com>
2025-03-10 12:43:36 +0300

    product-list created

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -17,10 +17,24 @@ type SampleData = {
   products: Product[];
 };
 
-const ProductList = ({ data, title }: { data: SampleData; title?: string }) => {
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: SampleData;
+  title?: string;
+  limit?: number;
+}) => {
+  const limitedData = limit ? data.products.slice(0, limit) : data.products;
   return (
     <div className="my-10">
       <h2 className="h2-bold mb-4">{title}</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {limitedData.map((product: Product) => (
+          <div key={product.slug}>{product.name}</div>
+        ))}
+      </div>
     </div>
   );
 };
